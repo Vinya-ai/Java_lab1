a1e776314494c838a9e248105b62b051e01cb519
Ivan <ivan.zhmurko.04@gmail.com>
2024-10-22 03:32:23 +0300

    add container and main

--- src/main/java/org/example/Container.java
+++ src/main/java/org/example/Container.java
@@ -1,2 +1,115 @@
-package org.example;public class Container {
+package org.example;
+/**
+ * Класс контейнер, позволяющий хранить произвольное количество объектов. Со свойством <b>head</b>.
+ * @author Иван Жмурко
+ * @param <T>  тип элементов в этом контейнере
+ */
+
+public class Container<T> {
+
+    /**
+     * Голова списка.
+     */
+    private Node<T> head;
+
+    /**
+     * Конструктор, создающий пустой контейнер.
+     */
+    public Container() {
+        head = null;
+    }
+
+    /**
+     * Внутренний статический класс, представляющий узел в связном списке.
+     * @param <T> тип данных, хранящихся в узле
+     */
+    private static class Node<T> {
+        /**
+         * Данные, хранящиеся в узле.
+         */
+        T data;
+        /**
+         * Следующий узел в списке.
+         */
+        Node<T> next;
+
+        /**
+         * Конструктор узла с указанными данными.
+         * @param data - значение, которые будет храниться в узле
+         */
+        Node(T data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    /**
+     * Функция добавления нового элемента в конец контейнера.
+     * @param data - значение, которые нужно добавить
+     * @see Node#data
+     */
+    public void add(T data) {
+        Node<T> newNode = new Node<T>(data);
+        Node<T> cNode = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (cNode.next != null) {
+                cNode = cNode.next;
+            }
+            cNode.next = newNode;
+        }
+    }
+
+    /**
+     * Функция получения элемента списка по его индексу
+     * @param index индекс элемента для получения
+     * @return возвращает элемент по указанному индексу
+     * @throws IndexOutOfBoundsException если индекс выходит за пределы
+     */
+    public T get(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Index: " + index);
+        }
+        Node<T> cNode = head;
+        for (int i = 0; i < index; i++) {
+            cNode = cNode.next;
+        }
+        return cNode.data;
+    }
+
+
+    /**
+     * Функция удаляния элемента по указанному индексу.
+     * @param index индекс элемента для удаления
+     * @throws IndexOutOfBoundsException если индекс выходит за пределы
+     */
+    public void remove(int index) {
+        Node<T> cNode = head;
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Index: " + index);
+        }
+        if (index == 0) {
+            head = head.next;
+        } else {
+            for (int i = 0; i < index - 1; i++) {
+                cNode = cNode.next;
+            }
+            cNode.next = cNode.next != null ? cNode.next.next : null;
+        }
+    }
+
+    /**
+     * Функция получения количества элементов в контейнере.
+     * @return возвращает размер контейнера
+     */
+    public Integer size() {
+        Integer i = 0;
+        Node<T> cNode = head;
+        while (cNode != null) {
+            i++;
+            cNode = cNode.next;
+        }
+        return i;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Container<String> container = new Container<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Добавление элементов
+        container.add("Wife");
+        container.add("Thing");
+        container.add("Virus");
+
+        // Получение элементов
+        System.out.println("Element at index 0: " + container.get(0));
+        System.out.println("Element at index 1: " + container.get(1));
+        System.out.println("Element at index 2: " + container.get(2));
+
+        // Удаление элемента
+        container.remove(1);
+        System.out.println("After removing element at index 1:");
+        System.out.println("Element at index 0: " + container.get(0));
+        System.out.println("Element at index 1: " + container.get(1));
+
+        // Размер контейнера
+        System.out.println("Size of container: " + container.size());
+
     }
 }
